f21184e2c794255b4e919b5fc20d6873c362bc3f
KidPudel <I.Kupchinenko@gmail.com>
2022-06-04 17:14:48 +0300

    Global usings for convenience

---
+++ AdventureTimeExplorer/GlobalUsings.cs
@@ -0,0 +1,5 @@
+﻿global using CommunityToolkit.Mvvm.ComponentModel;
+global using CommunityToolkit.Mvvm.Input;
+global using AdventureTimeExplorer.Models;
+global using AdventureTimeExplorer.ViewModels;
+global using AdventureTimeExplorer.Interfaces;
