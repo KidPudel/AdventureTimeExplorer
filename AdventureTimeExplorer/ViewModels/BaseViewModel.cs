6699395419df6378eb68d93a69b10036f099cfac
KidPudel <I.Kupchinenko@gmail.com>
2022-06-04 17:10:49 +0300

    Base class that handles notifications (helps with ui)

---
+++ AdventureTimeExplorer/ViewModels/BaseViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventureTimeExplorer.ViewModels;
+public partial class BaseViewModel : ObservableObject
+{
+    public BaseViewModel()
+    {
+
+    }
+
+    [ObservableProperty]
+    [AlsoNotifyChangeFor(nameof(IsNotBusy))]
+    private bool isBusy;
+
+    [ObservableProperty]
+    private string title;
+
+    public bool IsNotBusy => !IsBusy;
+}
