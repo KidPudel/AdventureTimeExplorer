49bf7dd94b2f6d46063af7c94210532a362a1e82
KidPudel <I.Kupchinenko@gmail.com>
2022-06-04 17:10:15 +0300

    Created a model for character response data

---
+++ AdventureTimeExplorer/Models/CharacterModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventureTimeExplorer.Models
+{
+    public class CharacterModel
+    {
+        public int id { get; set; }
+        public string slug { get; set; }
+        public string displayName { get; set; }
+        public string fullName { get; set; }
+        public string species { get; set; }
+        public string sex { get; set; }
+        public string[] quotes { get; set; }
+        public string sprite { get; set; }
+    }
+
+}
