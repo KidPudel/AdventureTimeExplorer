a4dea7c6082db4a72ca77beedd4c4a2d95c2ba01
KidPudel <I.Kupchinenko@gmail.com>
2022-06-04 17:12:05 +0300

    Service that retrieves adventure time data from REST API by sally (https://github.com/sally)

---
+++ AdventureTimeExplorer/Interfaces/ICharactersService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventureTimeExplorer.Interfaces
+{
+    /// <summary>
+    /// Class that calls to adventure time api (thank u sally https://github.com/sally)
+    /// </summary>
+    /// <returns></returns>
+    public interface ICharactersService
+    {
+
+        /// <summary>
+        /// Method that gets all characters
+        /// </summary>
+        /// <returns></returns>
+        Task<List<CharacterModel>> GetCharacters();
+    }
+}
---
+++ AdventureTimeExplorer/Services/CharacterService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventureTimeExplorer.Services
+{
+    public class CharacterService : ICharactersService
+    {
+        private HttpClient httpClient;
+        public CharacterService()
+        {
+            httpClient = new HttpClient();
+        }
+
+        private List<CharacterModel> Characters = new();
+        public async Task<List<CharacterModel>> GetCharacters()
+        {
+            // request path to get the list of AT chracters
+            var path = new Uri("https://adventure-time-api.herokuapp.com/api/v1/characters");
+
+            var response = await httpClient.GetAsync(path);
+
+            if (response.IsSuccessStatusCode)
+            {
+                Characters = await response.Content.ReadFromJsonAsync<List<CharacterModel>>();
+            }
+
+            return Characters;
+        }
+    }
+}
